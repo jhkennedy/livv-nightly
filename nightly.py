938117da0193bb2cd243de2d2a6a0eeff287049a
Joseph H Kennedy <kennedyjh@ornl.gov>
2015-09-04 14:44:53 -0400

    Begin building nightly test suite

---
+++ nightly.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+This script is designed to: 
+    1. Execute CISM's BATS
+    1. Analyze the BATS output with LIVVkit
+    3. Collate the new LIVVkit analysis with previous analyses
+    4. Generate a webpage for all current analyses.
+
+When combined with CRON, this will allow for nightly testing and analysis of
+CISM with LIVVkit.
+"""
+
+
