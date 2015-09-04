82f2138ec0d052190559241b8f17142c37ff1aaf
Joseph H Kennedy <kennedyjh@ornl.gov>
2015-09-04 16:51:49 -0400

    Find CISM and LIVVkit, then print commit hashes.

--- nightly.py
+++ nightly.py
@@ -11,4 +11,30 @@ When combined with CRON, this will allow for nightly testing and analysis of
 CISM with LIVVkit.
 """
 
+import argparse
+import subprocess
 
+
+# setup our input argument parser
+parser = argparse.ArgumentParser(description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('-c', '--cism', default='./cism',
+        help='The location of CISM.')
+parser.add_argument('-l', '--livv', default='./livv',
+        help='The location of LIVVkit.')
+
+
+args = parser.parse_args()
+
+def main():
+    livv_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=args.livv).strip()
+    cism_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=args.cism).strip()
+    
+    print("LIVVkit commit hash: "+livv_hash)
+    print("CISM commit hash:    "+cism_hash)
+
+
+if __name__=='__main__':
+    args = parser.parse_args()
+    main()
