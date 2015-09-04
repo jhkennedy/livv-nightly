52f57d64f2bfe6c864cc269013aa79dd625c8ba8
Joseph H Kennedy <kennedyjh@ornl.gov>
2015-09-04 17:58:54 -0400

    Check LIVV and CISM directories are correct.

--- nightly.py
+++ nightly.py
@@ -11,6 +11,8 @@ When combined with CRON, this will allow for nightly testing and analysis of
 CISM with LIVVkit.
 """
 
+import os
+import sys
 import argparse
 import subprocess
 
@@ -19,15 +21,43 @@ import subprocess
 parser = argparse.ArgumentParser(description=__doc__,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument('-c', '--cism', default='./cism',
+def normal_existing_dir(path):
+    path = os.path.normpath(path)
+    if not os.path.isdir(path):
+        print('Error! Directory does not exist: \n    '+path)
+        sys.exit(1)
+    return path
+
+def livv_type(path):
+    path = normal_existing_dir(path)
+    if not os.path.isdir(path+os.sep+'.git'):
+        print('Error! LIVVkit directory is not a git repository: \n    '+path)
+        print('Cannot determine current LIVVkit commit hash.')
+        sys.exit(1)
+    if not (os.path.isfile(path+os.sep+'livv.py')):
+        print('Error! No livv.py script found in LIVVkit directory: \n    '+path)
+        sys.exit(1)
+    return path
+
+def cism_type(path):
+    path = normal_existing_dir(path)
+    if not os.path.isdir(path+os.sep+'.git'):
+        print('Error! CISM directory is not a git repository: \n    '+path)
+        print('Cannot determine current CISM commit hash.')
+        sys.exit(1)
+    if not os.path.isdir(path+os.sep+'cism_driver'):
+        print('Error! CISM directory does not contain a cism_driver subdirectory: \n    '+path)
+        print('Cannot build CISM.')
+        sys.exit(1)
+    return path
+
+parser.add_argument('-c', '--cism', default='./cism', type=cism_type,
         help='The location of CISM.')
-parser.add_argument('-l', '--livv', default='./livv',
+parser.add_argument('-l', '--livv', default='./livv', type=livv_type,
         help='The location of LIVVkit.')
 
-
-args = parser.parse_args()
-
 def main():
+
     livv_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=args.livv).strip()
     cism_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=args.cism).strip()
     
