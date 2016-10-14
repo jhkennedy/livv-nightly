e2109a5178c80c6b9619955de766a2796dee0d43
Joseph H Kennedy <kennedyjh@ornl.gov>
2016-10-14 16:10:15 -0400

    Update setup script

--- setup.sh
+++ setup.sh
@@ -8,11 +8,11 @@ then
     # Clone CISM
     git clone https://github.com/ACME-Climate/cism-piscees.git cism
     # Clone LIVV
-    git clone https://github.com/ACME-Climate/LIVV.git livv
+    git clone https://github.com/ACME-Climate/LIVV.git LIVVkit
 else
     # Clone CISM
     git clone https://github.com/CISM/cism.git cism
     # Clone LIVV
-    git clone https://github.com/LIVVkit/LIVVkit.git livv
+    git clone https://github.com/LIVVkit/LIVVkit.git LIVVkit
 fi
 
